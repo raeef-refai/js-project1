27fac9d49d1adb8903e988f1c53b202bcad9f917
MohamadKayal <34998987+MohamadKayal@users.noreply.github.com>
2021-02-01 01:57:49 +0400

    Defined the first structure of the repo.
    to fetch the items in the JSON file and make a list of the projects

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
- 
-document.onload()=>{
 
+  // wrapping the whole script with onload function so it assures that the script won't work if the html page isn't fully loaded 
+   window.onload = () => {
+ 	// Better variable names -- decrlaring variables to use
 
- 	// Better variable names
-   const inputText = document.querySelector("#input");
+   const SearchInput = document.querySelector("#input");
    const SubmitButton = document.querySelector("#btn-submit");
-   const response = document.querySelector("#response");
+   const responseTag = document.querySelector("#response");
+  
+  // adding Event Listener for the button to make the search
 
+   SubmitButton.addEventListener('click',()=>{
+      getRepositories();
+   });
+
+// helper function to get the repos from github
    function getRepositories() {
 
-   	 if (inputText.value === '') {
+// conditional -- handling exceptions
+   	 if (SearchInput.value === '') {
    	 	
-       response.innerHTML = '<p>Please Enter A Username</p>'
+         alert('Please Enter Project name');
 
-   	 } else {
-   	 	// var url='https://api.github.com/users/'+theInput;
-   	 	// fetch(url)
-      //    .then(response => response.json())
+   	 } 
+       else {
 
-      //    .then((data) => {
-      //      data.forEach(user => {
-
-      //      	console.log(user)
-
-      //      	const mainDiv = document.createElement('div');
-
-      //      	const userName = document.createTextNode(user);
-
-      //      	mainDiv.appendChild(userName);
-
-      //      }) 
-      //    });
-       var url='https://api.github.com/users/'+inputText.value;
-       var response=fetch(url)
+         //declaring variables to handle the required data
+         var ProjectName; var RepoLink; var UserProfile;
+         // sending the request and fetching the JSON file
+         var url='https://api.github.com/search/repositories?q='+SearchInput.value;   	
+         var response=fetch(url)
          .then(response => response.json())
          .then(data => {
-         	
+
+         console.log(data);         	
          })
 
-		
+       //clearing the input after searching
+       SearchInput.value='';
+
    	 }
-
-
-
-
    }
 
-}
+};
