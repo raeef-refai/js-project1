b270dd8f2a5b02664d1a17bd41a68bd9b546c2dc
MohamadKayal <34998987+MohamadKayal@users.noreply.github.com>
2021-02-02 01:25:37 +0400

    Finished the search API requests in python

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import requests
 import json
 
-repoSearch=input('Enter the rapository name:\n')
+repoSearch=input('Enter the repository name:\n')
 url="https://api.github.com/search/repositories?q=%s&page=1&per_page=10"%repoSearch
-r=requests.get(url).json()
-for item in r:
-	temp=json.loads(item)
-	print(temp["items"])
-	# UserProfile=item.owner.url
-	# ProjectName=item.full_name;
-	# RepoLink = item.html_url;
-	# Description = item.description;
-	# mainOutput= "\n UserProfile is: %s, and project name is: %s, and RepoLink is: %s, \nand its about: %s" % (UserProfile,ProjectName,RepoLink,Description)
-	# print(mainOutput)
-	
+response=requests.get(url)
+data=response.json()
+for item in data['items']:
+
+	UserProfile=item['owner']['url']
+	ProjectName=item['full_name'];
+	RepoLink = item['html_url'];
+	Description = item['description'];
+	mainOutput= "\n UserProfile link is: %s, and project name is: %s, and RepoLink is: %s, \nand its about: %s\n\n" % (UserProfile,ProjectName,RepoLink,Description)
+	print(mainOutput)
 
